171d05ec00f6d8fa46bd818c81c5bd43243640c6
HellenIwata <hellen.iwata001@gmail.com>
2021-10-28 12:08:25 -0300

    Adicionando exercício da aula 02 de JS

---
+++ atividades/JS/Aula02/main.js
@@ -0,0 +1,9 @@
+let nome = 'Fulano'
+let idade = 20
+let altura = 1.90
+let peso = 85
+let IMC
+
+IMC = peso/(altura*altura)
+
+console.log(`Olá, ${nome}, o seu IMC é: ${IMC}. A sua altura é: ${altura} e o seu peso é: ${peso}`)
